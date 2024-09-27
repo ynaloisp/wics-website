dec879ff3113a9b34f1dff668dd2b1cce27c16ed
Ynalois Pangilinan <ynaloispangilinan@gmail.com>
2024-09-26 21:59:03 -0400

    fixed navbar dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -69,7 +69,7 @@ export default function Navbar() {
                     </li>
                     <li>
                       <Link
-                        className="block px-4 py-2 hover:bg-gray-200 text-sm"
+                        className="block px-4 py-2 hover:text-lightp text-sm"
                         href="/about/officers"
                         onClick={() => {
                           setAboutDropdownOpen(false);
