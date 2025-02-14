4bbb7e657179083398f6f5ea205cddd637117b7e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-02-13 22:53:20 -0500

    fixed gradients and text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -62,8 +62,8 @@ const config: Config = {
       },
       backgroundImage: {
         "background-gradient": "var(--background)",
-        "cardGradient": "var(--card-background)",
-        "buttonGradient": "var(--card-background)",
+        cardGradient: "var(--card-background)",
+        buttonGradient: "var(--button-background)",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
