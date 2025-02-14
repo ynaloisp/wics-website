d7d1f7a94c85908ee9927831bfd9a0003f8bd847
ynaloisp <ynaloispangilinan@gmail.com>
2025-02-14 16:17:19 -0500

    remove clerk/firebase related functions + start updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,171 +1,54 @@
 "use client";
-import { SignedOut, useAuth, useUser } from "@clerk/nextjs";
-import { db, auth, signInWithCustomToken } from "../firebase";
 import Image from "next/image";
-import { doc, setDoc, getDoc } from "firebase/firestore";
-import { useState, useEffect } from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogClose,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { MoveUpRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sparkles, ArrowRight } from "lucide-react";
 
 export default function Home() {
-  // const { getToken, userId } = useAuth();
-  const { user } = useUser();
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [emplid, setEmplid] = useState("");
-
-  // console.log("User ID:", userId);
-  // console.log("User:", user);
-  // console.log("User email:", user?.primaryEmailAddress?.emailAddress);
-  // console.log("User name:", `${user?.firstName} ${user?.lastName}`);
-
-  useEffect(() => {
-    async function checkUserEMPLID() {
-      if (user) {
-        const userDocRef = doc(db, "users", user.id);
-        const userDoc = await getDoc(userDocRef);
-
-        if (userDoc.exists()) {
-          const userData = userDoc.data();
-          if (!userData.emplid) {
-            setIsDialogOpen(true);
-          }
-          if (userData.points === undefined) {
-            await setDoc(userDocRef, { points: 0 }, { merge: true });
-          }
-        } else {
-          // If the user document does not exist, create it with initial data
-          const userData = {
-            email: user.emailAddresses[0]?.emailAddress || "",
-            fullname: `${user.firstName} ${user.lastName}` || "",
-            emplid: "",
-            points: 0,
-          };
-
-          // Sanitize data
-          if (userData.email && userData.fullname) {
-            await setDoc(userDocRef, userData);
-            setIsDialogOpen(true);
-          } else {
-            console.error("Invalid user data:", userData);
-          }
-        }
-      }
-    }
-
-    checkUserEMPLID();
-  }, [user]);
-
-  function storeEMPLID(EMPLID: string) {
-    // const emplidPattern = /^\d{8}$/; // Regular expression to match exactly 8 digits
-
-    // if (!emplidPattern.test(EMPLID)) {
-    //   console.error("Invalid EMPLID. It must be exactly 8 digits.");
-    //   return;
-    // }
-
-    if (user) {
-      setDoc(doc(db, "users", user.id), { emplid: EMPLID }, { merge: true })
-        .then(() => {
-          console.log("EMPLID stored successfully");
-          setIsDialogOpen(false);
-        })
-        .catch((error) => {
-          console.error("Error storing EMPLID:", error);
-        });
-    } else {
-      console.error("User is null or undefined");
-    }
-  }
-
-  // const storeUserData = async (firebaseUser: FirebaseUser) => {
-  //   try {
-  //     await setDoc(doc(db, "users", firebaseUser.uid), {
-  //       uid: firebaseUser.uid,
-  //       email: user?.primaryEmailAddress?.emailAddress,
-  //       fullname: `${user?.firstName} ${user?.lastName}`,
-  //     });
-  //     console.log("User data stored successfully");
-  //   } catch (error) {
-  //     console.error("Error storing user data:", error);
-  //   }
-  // };
-
-  // const signIntoFirebaseWithClerk = async () => {
-  //   const token = await getToken({ template: "integration_firebase" });
-
-  //   const userCredentials = await signInWithCustomToken(auth, token || "");
-  //   console.log("User:", userCredentials.user);
-
-  //   // Store user data in Firestore
-  //   storeUserData(userCredentials.user);
-  // };
-
-  // // Call the sign-in function when the component mounts
-  // React.useEffect(() => {
-  //   signIntoFirebaseWithClerk();
-  // }, []);
-
   return (
-    <div className="bg-[#fef8f8] min-h-screen pt-16">
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogTrigger asChild>
-          <button style={{ display: "none" }}>Open Dialog</button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogTitle className="text-pink">Enter Your EMPLID</DialogTitle>
-          <DialogDescription className="text-black">
-            Please enter your EMPLID in order to track your points for a prize.
-          </DialogDescription>
-          <input
-            type="text"
-            placeholder="Enter EMPLID"
-            value={emplid}
-            onChange={(e) => setEmplid(e.target.value)}
-            className="w-full p-2 my-4 border-2 border-pink rounded-md"
-          />
-          <div className="flex justify-between">
-            <DialogClose asChild>
-              <button
-                onClick={() => setIsDialogOpen(false)}
-                className="text-pink bg-white border-pink border-2 rounded-md p-2"
-              >
-                I don&apos;t have an EMPLID
-              </button>
-            </DialogClose>
-            <DialogClose asChild>
-              <button
-                onClick={() => {
-                  storeEMPLID(emplid);
-                  setIsDialogOpen(false);
-                }}
-                className="text-pink bg-white border-pink border-2 rounded-md p-2"
-              >
-                Save EMPLID
-              </button>
-            </DialogClose>
+    <div className="min-h-screen bg-gradient-to-r from-[#fdf2f8] via-white to-[#fdf2f8]">
+      <div className="pt-16 pb-16 px-4">
+        <div className="container mx-auto">
+          <div className="flex flex-cols-1 lg:flex-cols-2 gap-40 items-center justify-center">
+            <div className="space-y-6">
+              <div className="inline-block">
+                <div className="flex items-center space-x-2 bg-[#fce7f3] rounded-full px-4 py-1">
+                  <Sparkles className="w-4 h-4 text-hotpink" />
+                  <span className="text-sm text-hotpink font-semibold">
+                    Hunter College WiCS
+                  </span>
+                </div>
+              </div>
+              <h1 className="text-5xl font-bold leading-tight">
+                Where Women in Tech
+                <span className="block text-hotpink">Shape the Future</span>
+              </h1>
+              <p className="text-lg text-grey max-w-2xl">
+                Join a vibrant community of tech enthusiasts, innovators, and
+                future leaders at Hunter College.
+              </p>
+              <div className="flex items-center space-x-4">
+                <a href="https://forms.gle/KuWM3UqUjjrPANCr7" target="_blank">
+                  <Button className="bg-hotpink hover:bg-[#be185d] px-6">
+                    Join WiCS
+                    <ArrowRight className="ml-2 w-4 h-4" />
+                  </Button>
+                </a>
+              </div>
+            </div>
+
+            <div className="relative">
+              <div className="absolute inset-0 bg-gradient-to-r from-[#FBCEEC] to-[#fdf2f8] rounded-2xl transform rotate-3 " />
+              <Image
+                src="/images/event/cookie-decorating-team.jpg"
+                alt="team"
+                width={700}
+                height={500}
+                className="relative rounded-2xl transform -rotate-3 transition-transform hover:rotate-0"
+              />
+            </div>
           </div>
-        </DialogContent>
-      </Dialog>
-      <div className="flex justify-center py-14 px-6">
-        <Image
-          src="/images/WiCS-Logo.png"
-          alt="WiCS Logo"
-          width={500}
-          height={200}
-          className="rounded-3xl shadow-img"
-        />
-      </div>
-      <div className="flex justify-center py-8 px-6">
-        <h1 className="sm:text-xl md:text-3xl text-2xl font-bold text-center">
-          &lt;/ WOMEN IN COMPUTER SCIENCE &gt; <br /> @ HUNTER COLLEGE
-        </h1>
+        </div>
       </div>
 
       <div className="container mx-auto w-4/5 pb-20 flex items-center">
