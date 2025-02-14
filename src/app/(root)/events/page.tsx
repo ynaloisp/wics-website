076800a1d30d5c008283f1a3f8c1ff9a2a095f42
anitaprova <104943181+anitaprova@users.noreply.github.com>
2025-02-13 21:46:39 -0500

    added first card to past events

--- src/app/(root)/events/page.tsx
+++ src/app/(root)/events/page.tsx
@@ -15,7 +15,7 @@ import { Calendar, Clock, MapPin } from "lucide-react";
 
 export default function Events() {
   return (
-    <div className="ml-[10%] mr-[10%] mt-[5%] mb-[10%] space-y-16">
+    <div className="font-inter ml-[10%] mr-[10%] mt-[5%] mb-[10%] space-y-16">
       <div className="relative">
         <div className="absolute inset-0 rotate-3 rounded-2xl bg-gradient-to-br from-[#FBCEEC] to-[#FDF2F8]"></div>
         <div className="bg-white flex flex-col gap-5 rounded-2xl p-10 pb-[5%] -rotate-1">
@@ -27,7 +27,7 @@ export default function Events() {
         </div>
       </div>
 
-      <div className="font-inter font-bold text-2xl space-y-4">
+      <div className="font-bold text-2xl space-y-4">
         <h1>Upcoming Event</h1>
         <Card className="space-y-3 border-none">
           <CardHeader className="p-0">
@@ -70,15 +70,41 @@ export default function Events() {
         </Card>
       </div>
 
-      <div className="font-inter font-bold text-2xl space-y-2">
-        Past Event Highlights
+      <div className="space-y-4">
+        <h1 className="font-bold text-2xl">Past Event Highlights</h1>
+        <div className="grid grid-cols-3">
+          <Card className="p-0 rounded-xl">
+            <CardHeader className="">
+              <Image
+                src="https://th.bing.com/th/id/OIP.2N3yUqpMYG6VHxj1maGVpAHaEo?rs=1&pid=ImgDetMain"
+                alt="External Image"
+                unoptimized
+                width={100}
+                height={200}
+                className="w-full h-[25vh] rounded-lg"
+              />
+            </CardHeader>
+            <CardContent>
+              <p className="font-bold">Break Through Tech Info Session</p>
+              <p className="text-sm text-500 text-lightg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </CardContent>
+            <CardFooter>
+              <button className="font-bold bg-white rounded-sm w-full border border-1 p-2">
+                View Recap
+              </button>
+            </CardFooter>
+          </Card>
+        </div>
+        <button className="bg-pink p-2 pl-4 pr-4 font-bold rounded-sm text-grey">
+          See More
+        </button>
       </div>
 
       <div className="bg-cardGradient rounded-2xl p-10 border border-1">
         <div className="flex flex-col gap-4 items-center text-center space-y-4">
-          <h1 className="font-inter font-bold text-2xl">
-            Want to Host an Event?
-          </h1>
+          <h1 className="font-bold text-2xl">Want to Host an Event?</h1>
           <p>
             Have an idea for a workshop, tech talk, or any other event? We’d
             love to hear from you!
