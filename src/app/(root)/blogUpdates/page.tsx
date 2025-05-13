4c4595b7f4791eae3975222c471b5056b055fb99
SelinaZheng <selinazhenglx@gmail.com>
2025-05-12 20:30:56 -0400

    ...

--- src/app/(root)/blogUpdates/page.tsx
+++ src/app/(root)/blogUpdates/page.tsx
@@ -1,23 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRef } from "react";
+import { getFirestore } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
+import "firebase/firestore";
 export default function Blog() {
+    const eventNameRef = useRef<HTMLInputElement>(null);
+    const firestore = getFirestore();
+    const ref = collection(firestore, "blogPosts");
+    const handleSave = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Event Name:", eventNameRef.current?.value);
+        try {
+            await addDoc(ref, {
+                eventName: eventNameRef.current?.value,
+            });
+            const statusMessage = document.getElementById("statusMessage");
+            if (statusMessage) {
+                statusMessage.innerHTML =
+                    "<p class='text-green-500'>Event name saved successfully!</p>";
+            }
+        } catch (error) {
+            console.error("Error saving event name:", error);
+        }
+    };
     return (
         <div>
-            <form className="space-y-4 flex flex-col items-center padding-10">
+            <form
+                className="space-y-4 flex flex-col items-center padding-10"
+                onSubmit={handleSave}
+            >
                 <h1 className="text-2xl font-bold">Blog Updates</h1>
+                <label htmlFor="eventName" className="text-xl">
+                    Enter Event Name
+                </label>
                 <input
                     type="text"
                     id="eventName"
                     placeholder="Event Name"
+                    ref={eventNameRef}
                     required
                 />
-                <input type="date" id="eventDate" required />
-                <input type="time" id="eventTime" required />
-                <input type="file" id="flyer" accept="image/*" required />
-                <textarea
-                    id="eventDetails"
-                    placeholder="Event Details"
-                    required
-                ></textarea>
-                <button type="submit">Submit</button>
+                <Button
+                    type="submit"
+                    className="bg-buttonGradient font-semibold rounded-lg p-2 pl-5 pr-5 text-grey mt-[2%]"
+                >
+                    Save
+                </Button>
             </form>
 
             <div id="statusMessage"></div>
--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getAuth, signInWithCustomToken } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
-  authDomain: "wics-4809f.firebaseapp.com",
-  projectId: "wics-4809f",
-  storageBucket: "wics-4809f.appspot.com",
-  messagingSenderId: "744034431803",
-  appId: "1:744034431803:web:ddba0aaeec1cbb5906fd1e",
-  measurementId: "G-GVEXD7LT2Y",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+    authDomain: "wics-4809f.firebaseapp.com",
+    projectId: "wics-4809f",
+    storageBucket: "wics-4809f.appspot.com",
+    messagingSenderId: "744034431803",
+    appId: "1:744034431803:web:ddba0aaeec1cbb5906fd1e",
+    measurementId: "G-GVEXD7LT2Y",
 };
 
 // Initialize Firebase
@@ -25,3 +25,5 @@ const auth = getAuth(app);
 // const analytics = getAnalytics(app);
 
 export { db, auth, signInWithCustomToken };
+
+export const firestore = getFirestore(app);
